e5159263cc31e6ae72e4e4216e013374c723bc01
lihongzhe <hongzheli@outlook.com>
2021-03-06 09:32:47 +0800

    add next greater number 1

---
+++ src/main/java/stack/NextGreaterElement1.java
@@ -0,0 +1,78 @@
+package stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * You are given two integer arrays nums1 and nums2 both of unique elements, where nums1 is a subset of nums2.
+ *
+ * Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+ *
+ * The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2.
+ * If it does not exist, return -1 for this number.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+ * Output: [-1,3,-1]
+ * Explanation:
+ * For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+ * For number 1 in the first array, the next greater number for it in the second array is 3.
+ * For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+ * Example 2:
+ *
+ * Input: nums1 = [2,4], nums2 = [1,2,3,4]
+ * Output: [3,-1]
+ * Explanation:
+ * For number 2 in the first array, the next greater number for it in the second array is 3.
+ * For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums1.length <= nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 10to4
+ * All integers in nums1 and nums2 are unique.
+ * All the integers of nums1 also appear in nums2.
+ */
+public class NextGreaterElement1 {
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i =0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            Integer idx = map.get(nums1[i]);
+            res[i] = -1;
+            for (int j = idx+1; j < nums2.length; j++) {
+                if (nums2[j] > nums1[i]) {
+                    res[i] = nums2[j];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    //find out the next greater during iteration
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
+}
